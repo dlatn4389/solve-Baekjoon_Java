0504dbd2d7a286f3af3433251b5408edf7cfa410
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2020-12-31 19:23:22 +0900

    사칙연산

---
+++ Problem10869.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Problem10869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String two_integer = sc.nextLine();
+        String[] integers = two_integer.split(" ");
+        int a = Integer.parseInt(integers[0]);
+        int b = Integer.parseInt(integers[1]);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
