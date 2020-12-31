8201ee7de016360db214b1581cd0d65dc303a2f0
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2020-12-31 15:20:38 +0900

    강한친구 대한육군

---
+++ Problem10718.java
@@ -0,0 +1,13 @@
+public class Problem10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
+
+/*public class Main {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}*/
